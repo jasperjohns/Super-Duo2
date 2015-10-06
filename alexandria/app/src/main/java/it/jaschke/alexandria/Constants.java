b17bb3b78393f86719632cfb7aa6297e1fc7a22d
jasperjohns <@Eleeye4444>
2015-10-06 15:57:52 -0500

    definitions

---
+++ alexandria/app/src/main/java/it/jaschke/alexandria/Constants.java
@@ -0,0 +1,18 @@
+package it.jaschke.alexandria;
+
+/**
+ * Created by asaldanha on 10/5/2015.
+ */
+public class Constants {
+
+
+    public static String EAN = "EAN";
+    public static String BOOK_TITLE = "booktitle";
+    public static String BOOK_SUB_TITLE = "booksubtitle";
+    public static String BOOK_DESC = "bookdesc";
+    public static String BOOK_AUTHORS = "authors";
+    public static String BOOK_CATEGORIES = "bookcategory";
+    public static String BOOK_IMAGE_URL ="bookimgurl";
+
+
+}
